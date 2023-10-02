4c733497523fc1b966291e23c74221a62007e6cb
codesloth <djqmflsk@naver.com>
2023-10-02 12:32:02 +0900

    [추가] W13 백준 알고리즘

---
+++ W13/개인문제/BOJ_1012_유기농 배추.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+d = [(1,0),(-1,0),(0,1),(0,-1)]
+
+def dfs(x,y):
+    for dx,dy in d:
+        nx,ny = x+dx, y+dy
+        if 0 <= nx < n and 0 <= ny < m and g[nx][ny]:
+            g[nx][ny] ^= 1
+            dfs(nx,ny)
+    return 1
+
+for _ in range(int(input())):
+    n, m, k = map(int,input().split())
+    g = [[0]*m for __ in range(n)]
+    for ___ in range(k):
+        a, b = map(int,input().split())
+        g[a][b] ^= 1
+    t = 0
+    for i in range(n):
+        for j in range(m):
+            if g[i][j]:
+                g[i][j] ^= 1
+                t += dfs(i,j)
+    print(t)
