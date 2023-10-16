10628f7ff47c4f86d51e49602d8cd2483716987c
codesloth <djqmflsk@naver.com>
2023-10-16 16:13:32 +0900

    [추가] W15 백준 알고리즘

---
+++ W15/개인문제/BOJ_1929_소수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline()
+
+n1, n2 = map(int,input().split())
+total = 0
+
+for i in range(n1,n2+1):
+    t = 0
+    if i > 1:
+        for j in range(2,int(i**0.5)+1):
+            if i % j == 0:
+                t += 1
+                if t > 0: break
+        if t == 0:
+            print(i)
---
+++ W15/개인문제/BOJ_6571_피보나치 수의 개수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while 1:
+    n, m = map(int,input().split())
+    if n == 0 and m == 0: break
+    t = 0
+    a = 1
+    b = 1
+    while 1:
+        a,b = b,a+b
+        if a >= n: t += 1
+        if a > m: break
+    print(t-1)
